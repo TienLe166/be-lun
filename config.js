00bf30b64bdff10ada099a7664f6c1d8bfe3debc
TienLe166 <89723484+TienLe166@users.noreply.github.com>
2021-08-29 14:31:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    Mà sao em cứ thích chọc anh
+    Cứ thích làm anh thương bé`,
+    btnIntro: 'Bớt chọc tức anh lại',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
